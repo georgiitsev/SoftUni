77322a1eec66badcdad6e1a34b575a3746d26842
georgiitsev <ludiq_18@bv.bg>
2016-07-17 09:17:28 +0300

    StupidPasswordGenerator problem solved

---
+++ AdvancedLoop/StupidPasswordGenerator/StupidPasswordGenerator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace StupidPasswordGenerator
+{
+    class StupidPasswordGenerator
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int l = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j < n; j++)
+                {
+                    for (int k = 0; k < l; k++)
+                    {
+                        for (int m = 0; m < l; m++)
+                        {
+                            for (int p = 1; p <= n; p++)
+                            {
+                                if (p > i && p > j)
+                                {
+                                    string result = i.ToString() + j + (char)(k + 97) + (char)(m + 97) + p;
+                                    Console.Write(result + " ");
+                                }
+
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
