9f6a2788b276605cf5d1ac2c7ea2519d2f48a931
georgiitsev <ludiq_18@bv.bg>
2016-07-17 16:36:46 +0300

    All Problem  solved

---
+++ AdvancedLoop/18.MagicNumbers/MagicNumbers.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _18.MagicNumbers
+{
+    class MagicNumbers
+    {
+        static void Main()
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 9; i++)
+            {
+                for (int j = 1; j <= 9; j++)
+                {
+                    for (int k = 1; k <= 9; k++)
+                    {
+                        for (int l = 1; l <= 9; l++)
+                        {
+                            for (int m = 1; m <= 9; m++)
+                            {
+                                for (int p = 1; p <= 9; p++)
+                                {
+                                    if (i * j * k * l * m * p == number)
+                                    {
+                                        Console.Write("{0}{1}{2}{3}{4}{5} ", i, j, k, l, m, p);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ AdvancedLoop/SpecialNumbers/SpecialNumbers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SpecialNumbers
+{
+    class SpecialNumbers
+    {
+        static void Main()
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 9; i++)
+            {
+                for (int j = 1; j <= 9; j++)
+                {
+                    for (int l = 1; l <= 9; l++)
+                    {
+                        for (int m = 1; m <= 9; m++)
+                        {
+                            if (number % i == 0 && number % j ==0 && number % l ==0 && number % m ==0)
+                            {
+                                Console.Write("{0}{1}{2}{3} ",i,j,l,m);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
